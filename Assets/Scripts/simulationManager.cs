8c8141501d7de770d4d8364c6359146b73bec9cc
Sokdenumeros <sergi.curto@gmail.com>
2023-12-14 17:41:54 +0100

    Updated simulation manager to sort particles, update temperatures and use different ways of drawing the particles

--- Assets/Scripts/simulationManager.cs
+++ Assets/Scripts/simulationManager.cs
@@ -9,8 +9,6 @@ public class simulationManager : MonoBehaviour
     LinkedList<float> timeList;    
 
     public floatLoader temperatureManager;
-    private float[] temperature;
-    private float[] nextTemperature;
 
     public vectorLoader velocityManager;
     private Vector3[] velocity;
@@ -20,20 +18,61 @@ public class simulationManager : MonoBehaviour
     public Material fsmat;
     public Mesh quadmesh;
 
-    public 
+    //Variables to test the dataElement struct
+    dataElement temperatures;
+
+    struct dataElement{
+        public float[] previous;
+        public float[] next;
+        public float previousTime;
+        public float nextTime;
+        public int dimx, dimy, dimz;
+        public dataElement(float[] a, float[] b, int x, int y,int  z) {
+            previousTime = 0.0f;
+            nextTime = 0.0f;
+            previous = a;
+            next = b;
+            dimx = x;
+            dimy = y;
+            dimz = z;
+        }
+
+        public float sample(float t, float x, float y, float z){
+            float timeFactor = (t-previousTime)/(nextTime-previousTime);
+            float timesample0 = previous[ (int)y*dimy*dimx + (int)z*dimx + (int)x ];
+            float timesample1 = next[(int)y * dimy * dimx + (int)z * dimx + (int)x];
+            return (1-timeFactor)*timesample0 + timeFactor*timesample1;
+        }
+
+        public float sample(float t, Vector3 v){
+            float x = v.x;
+            float y = v.y;
+            float z = v.z;
+            float timeFactor = (t-previousTime)/(nextTime-previousTime);
+            float timesample0 = previous[ (int)y*dimy*dimx + (int)z*dimx + (int)x ];
+            float timesample1 = next[(int)y * dimy * dimx + (int)z * dimx + (int)x];
+            return (1-timeFactor)*timesample0 + timeFactor*timesample1;
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
         temperatureManager.initialize();
-        temperature = temperatureManager.getData();
-        nextTemperature = temperatureManager.getNextData();
+
         velocityManager.initialize();
         velocity = velocityManager.getData();
         nextVelocity = velocityManager.getNextData();
+
         timeList = new LinkedList<float>();
         timeList.AddLast(0.0f);
         timeList.AddLast(0.5f);
         timeList.AddLast(1.0f);
+
+        temperatures = new dataElement(temperatureManager.getData(),temperatureManager.getNextData(),dimx,dimy,dimz);
+        temperatures.previousTime = timeList.First.Value;
+        temperatures.nextTime = timeList.First.Next.Value;
+
         instantiateParticles();
     }
 
@@ -46,8 +85,12 @@ public class simulationManager : MonoBehaviour
             timeList.RemoveFirst();
             timeList.AddLast(time);
 
-            temperature = temperatureManager.newTimeData(time);
-            nextTemperature = temperatureManager.getNextData();
+            temperatures.previousTime = timeList.First.Value;
+            temperatures.nextTime = timeList.First.Next.Value;
+
+            temperatures.previous = temperatureManager.newTimeData(time);
+            temperatures.next = temperatureManager.getNextData();
+
             velocity = velocityManager.newTimeData(time);
             nextVelocity = velocityManager.getNextData();
 
@@ -56,6 +99,7 @@ public class simulationManager : MonoBehaviour
 
             //instObjects();
             instantiateParticles();
+            //instantiateParticleObjects();
         }
         updateParticles();
         paintParticles();
@@ -67,7 +111,7 @@ public class simulationManager : MonoBehaviour
         MeshRenderer me;
         Material mat;
         float exp;
-        float[] data = temperature;
+        float[] data = temperatures.previous;
         for (int z = 0; z < dimz; z += 1)
             for (int y = 0; y < dimy; y += 1)
                 for (int x = 0; x < dimx; x += 1)
@@ -105,12 +149,11 @@ public class simulationManager : MonoBehaviour
 
         particles = new List<partInfo>(dimx*dimy*dimz);
         float exp;
-        for (int z = 0; z < dimz; z += 1)
-            for (int y = 0; y < dimy; y += 1)
-                for (int x = 0; x < dimx; x += 1)
-                {
-                    exp = (temperature[z * dimy * dimx + y * dimx + x] - 300) / 1300;
-                    if (exp > 0) particles.Add(new partInfo(new Vector3(x, z, y) / 100, temperature[z * dimy * dimx + y * dimx + x]));
+        float[] temps = temperatures.previous;
+        //for (int z = 0; z < dimz; z += 1) for (int y = 0; y < dimy; y += 1) for (int x = 0; x < dimx; x += 1){
+        for (int z = dimz-1; z >= 0; z -= 1) for (int y = dimy-1; y >= 0; y -= 1) for (int x = dimx-1; x >= 0; x -= 1){
+                    exp = (temps[z * dimy * dimx + y * dimx + x] - 300) / 1300;
+                    if (exp > 0.5) particles.Add(new partInfo(new Vector3(x, z, y) / 100, temps[z * dimy * dimx + y * dimx + x]));
                 }
         
     }
@@ -126,15 +169,16 @@ public class simulationManager : MonoBehaviour
             pos = p.position*100.0f;
             pos = Vector3.Max(pos, Vector3.zero);
             pos = Vector3.Min(pos, big);
+            p.temperature = temperatures.sample(Time.time,pos);
             //INTERCHANGING Y AND Z BECAUSE INDICES WERE FLIPPED
-            p.position += ((1-factor)*velocity[(int)(pos.y * dimy * dimx + pos.z * dimx + pos.x)]+factor*nextVelocity[(int)(pos.y * dimy * dimx + pos.z * dimx + pos.x)])*Time.deltaTime;
+            //p.position += ((1-factor)*velocity[(int)(pos.y * dimy * dimx + pos.z * dimx + pos.x)]+factor*nextVelocity[(int)(pos.y * dimy * dimx + pos.z * dimx + pos.x)])*Time.deltaTime;
             particles[i] = p;
-
         }
     }
 
     private void paintParticles() {
-        RenderParams rp = new RenderParams(fsmat);
+        GameObject o = Instantiate(fireParticle, new Vector3(0,0,0), Quaternion.identity);
+        RenderParams rp = new RenderParams(o.GetComponent<MeshRenderer>().material);
         Matrix4x4 scaleMatrix = Matrix4x4.Scale(new Vector3(0.1f,0.1f,0.1f));
         float exp;
         Color c;
@@ -143,10 +187,27 @@ public class simulationManager : MonoBehaviour
             exp = (particles[i].temperature - 300) / 1300;
             c = Color.Lerp(Color.red,Color.yellow,exp);
             rp.material.color = Color.Lerp(Color.clear,c,exp*2);
-            rp.material.color = c;
+            //rp.material.color = c;
             //rp = new RenderParams(m);
             Graphics.RenderMesh(rp, quadmesh, 0, Matrix4x4.Translate(particles[i].position)*scaleMatrix);
         }
+        Destroy(o);
         //Graphics.DrawMeshInstancedIndirect(instanceMesh, subMeshIndex, instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), argsBuffer);
+    }
+
+    private void instantiateParticleObjects() {
+        GameObject o;
+        MeshRenderer me;
+        float exp;
+        Color c;
+        for(int i = 0; i < particles.Count; ++i){
+            exp = (particles[i].temperature - 300) / 1300;
+            o = Instantiate(fireParticle, particles[i].position, Quaternion.identity);
+            me = o.GetComponent<MeshRenderer>();
+            c = Color.Lerp(Color.red,Color.yellow,exp);
+            me.material.color = Color.Lerp(Color.clear,c,exp*2);
+            fsmat.color = Color.Lerp(Color.clear,c,exp*2);
+            me.material = fsmat;
+        }
     }
 }
--- Misc/fdstoraw.py
+++ Misc/fdstoraw.py
@@ -10,16 +10,30 @@ temp, grid = slc.to_global(return_coordinates=True, masked=True)
 
 # Get the times
 times = slc.times
-
+#0 2 3 1 maybe?
 temp= np.transpose(temp, (0, 3, 2, 1))
 
+# Get the shape of the temperature array
+shape = temp.shape
 # Loop through each time step
 for i, time in enumerate(times):
     # Flatten the temperature data
     flattened_temp = temp[i].flatten()
 
-    # Get the shape of the temperature array
-    shape = temp.shape
-
     # Write to a raw file for each time step, format of the filename is the following dimZ.dimY.dimX
-    flattened_temp.astype(np.float32).tofile('outputFds' + f'_T{time:.1f}_.' + str(shape[1]) + '.' + str(shape[2]) + '.' + str(shape[3]) + '.raw')
+    flattened_temp.astype(np.float32).tofile('temperature' + f'_T{time:.1f}_.' + str(shape[1]) + '.' + str(shape[2]) + '.' + str(shape[3]) + '.raw')
+
+uVelocity, grid = sim.slices[0].to_global(return_coordinates=True, masked=True)
+vVelocity, grid = sim.slices[1].to_global(return_coordinates=True, masked=True)
+wVelocity, grid = sim.slices[2].to_global(return_coordinates=True, masked=True)
+
+velocities = np.stack([uVelocity, vVelocity, wVelocity], axis=4)
+#0 2 3 1 4 maybe?
+velocities = np.transpose(velocities, (0, 3, 2, 1, 4))
+
+shape = velocities.shape
+
+for i, time in enumerate(times):
+
+    flattened_velocity = velocities[i].flatten()
+    flattened_velocity.astype(np.float32).tofile('velocity' + f'_T{time:.1f}_.' + str(shape[1]) + '.' + str(shape[2]) + '.' + str(shape[3]) + '.raw')
